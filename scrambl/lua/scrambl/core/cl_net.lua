d860412e33d349c41d6de0f9fb3066107a41294c
Define <166741214+DefineRust@users.noreply.github.com>
2025-02-26 23:51:45 +1000

    Add sound

--- scrambl/lua/scrambl/core/cl_net.lua
+++ scrambl/lua/scrambl/core/cl_net.lua
@@ -4,4 +4,10 @@ net.Receive( "scrambl_addtext", function()
 	local msg = net.ReadString()
 	chat.AddText( ( cfg.prefix_color or Color( 100, 200, 100 ) ), cfg.prefix, color_white, msg )
 
+	if cfg.sound then
+
+		surface.PlaySound( cfg.sound )
+
+	end
+
 end)
