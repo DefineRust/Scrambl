dfa5f02e15ea293cd0897a78e1e86b42203119e2
Define <166741214+DefineRust@users.noreply.github.com>
2025-02-27 00:26:54 +1000

    Change sound logic

--- scrambl/lua/scrambl/core/cl_net.lua
+++ scrambl/lua/scrambl/core/cl_net.lua
@@ -2,9 +2,10 @@ local cfg = scrambl.cfg
 net.Receive( "scrambl_addtext", function()
 
 	local msg = net.ReadString()
-	chat.AddText( ( cfg.prefix_color or Color( 100, 200, 100 ) ), cfg.prefix, color_white, msg )
+	local playSound = net.ReadBool()
+	chat.AddText( Color( 100, 200, 100 ), cfg.prefix, color_white, msg )
 
-	if cfg.sound then
+	if ( playSound and cfg.sound ) then
 
 		surface.PlaySound( cfg.sound )
 
