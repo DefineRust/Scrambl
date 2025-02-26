0932ae8aa2c22bfa0df5638911bb8bffebaf4cba
Define <166741214+DefineRust@users.noreply.github.com>
2025-02-27 00:27:07 +1000

    Sound logic

--- scrambl/lua/scrambl/core/sv_net.lua
+++ scrambl/lua/scrambl/core/sv_net.lua
@@ -1,10 +1,11 @@
 local cfg = scrambl.cfg
 
 util.AddNetworkString( "scrambl_addtext" )
-function scrambl.AddText( msg, omit, only )
+function scrambl.AddText( msg, start, omit, only )
 
 	net.Start( "scrambl_addtext" )
 		net.WriteString( msg )
+		net.WriteBool( start )
 
 	if omit then
 		net.SendOmit( omit )
