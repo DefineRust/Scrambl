544d086b5b648bb6ed0c4e672d3089e095208962
Define <166741214+DefineRust@users.noreply.github.com>
2025-02-26 23:37:39 +1000

    prefix_color

--- scrambl/lua/scrambl/core/cl_net.lua
+++ scrambl/lua/scrambl/core/cl_net.lua
@@ -2,6 +2,6 @@ local cfg = scrambl.cfg
 net.Receive( "scrambl_addtext", function()
 
 	local msg = net.ReadString()
-	chat.AddText( Color( 100, 200, 100 ), cfg.prefix, color_white, msg )
+	chat.AddText( ( cfg.prefix_color or Color( 100, 200, 100 ) ), cfg.prefix, color_white, msg )
 
 end)
